d2d15212b6e98b81ff2a6ea391052ff135d7d588
Nirbhik16 <87673341+Nirbhik16@users.noreply.github.com>
2022-11-11 21:18:11 +0530

    Update 5.cpp

--- Arrays/Medium/5.cpp
+++ Arrays/Medium/5.cpp
@@ -2,42 +2,33 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> sol;
-        int count=0,sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(sum==k){
-                count++;
-            }
-            if(sol.find(sum-k)!=sol.end()){
-                count=sol[sum-k]+count;
-            }
-            if(sol.find(sum)!=sol.end()){
-                sol[sum]++;
-            }
-            else{
-                sol[sum]=1;
-            }
+        int n = nums.size(); 
+        
+        int prefix[n]; 
+        
+        prefix[0] = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            prefix[i] = nums[i] + prefix[i - 1];
         }
-        return count;
-    }
+        
+        unordered_map<int,int> mp; 
+        
+        int ans = 0; 
+        
+        for(int i = 0; i < n; i++) 
+        {
+            if(prefix[i] == k) 
+                ans++;    
+            if(mp.find(prefix[i] - k) != mp.end())
+            {
+                ans += mp[prefix[i] - k];
+            }
+            
+            mp[prefix[i]]++;
+        }
+        
+        return ans;
 
-/*
-    int subarraySum(vector<int>& nums, int k) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            int s=nums[i];
-            if(s==k){
-                count++;
-            }
-            for(int j=i+1;j<nums.size();j++){
-                s+=nums[j];
-                if(s==k){
-                    count++;
-                }
-            }
-        }
-        return count;
     }
-*/
 };
